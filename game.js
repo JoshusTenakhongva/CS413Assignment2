c59dc3977da951c3f450d87ce6ace085f6ff9868
Joshus Tenakhongva <jtt75@nau.edu>
2019-10-07 22:12:57 -0700

    Menu functionality added

--- game.js
+++ game.js
@@ -15,14 +15,6 @@ var tutorialScreen = new PIXI.Container();
 var containers = [ titleScreen, stage, creditsScreen, tutorialScreen ]; 
 
 stage.addChild( titleScreen ); 
-stage.addChild( gameplayScreen ); 
-stage.addChild( creditsScreen ); 
-stage.addChild( tutorialScreen );
-
-titleScreen.interactive = true; 
-stage.interactive = true; 
-creditsScreen.interactive = true; 
-tutorialScreen.interactive = true; 
 
 /*
 * Creating the objects for playing the game. 
@@ -33,21 +25,53 @@ var titleMenu = new PIXI.Sprite( PIXI.Texture.fromImage( "TitleScreen.png" ));
 var startButton = new PIXI.Sprite( PIXI.Texture.fromImage( "StartButton.png" )); 
 var tutorialButton = new PIXI.Sprite( PIXI.Texture.fromImage( "TutorialButton.png" )); 
 var creditsButton = new PIXI.Sprite( PIXI.Texture.fromImage( "CreditsButton.png" )); 
+var titleButton = new PIXI.Sprite( PIXI.Texture.fromImage( "TitleButton.png" )); 
 
 /*
 var enemySprite = PIXI.Texture.fromImage( "enemy.png" ); 
 var enemy = new PIXI.Sprite( enemySprite ); 
 
 /*
-* Add objects to containers 
+* Populate the title screen and create buttons
 */ 
 titleScreen.addChild( titleMenu ); 
+
+// Add the start button to the title screen and its functionality 
+titleScreen.addChild( startButton ); 
+startButton.interactive = true; 
+startButton.on( 'mousedown', startButtonClickHandler ); 
+
+// Add the tutorial button to the title screen and its functionality 
+titleScreen.addChild( tutorialButton ); 
+tutorialButton.interactive = true;
+tutorialButton.on( 'mousedown', tutorialButtonClickHandler );  
+
+// Add the credits button to the title screen and its functionality
+titleScreen.addChild( creditsButton ); 
+creditsButton.interactive = true; 
+creditsButton.on( 'mousedown', creditsButtonClickHandler ); 
+
+titleButton.interactive = true; 
+titleButton.on( 'mousedown', titleMenuButtonClickHandler ); 
+
+/*
+* Populate the tutorial screen 
+*/ 
+
+
+/* 
+* Populate the credits screen 
+*/ 
+
+
+/*
+* Populate the gameplay screen 
+*/ 
 gameplayScreen.addChild( cannon ); 
 
 /*
 Constant Variables
 */
-
 const cannonX = 400; 
 const cannonY = 300; 
 const bulletSpeed = 20; 
@@ -61,8 +85,12 @@ var yDirection;
 var angle; 
 var mousePosition = getMousePosition(); 
 var bulletShotFlag = false;
-var containerIndex; 
+var index; 
 var score = 0; 
+
+var tutorialVisited = false; 
+var creditsVisited = false; 
+var gameVisited = false; 
 
 /*
 Initializing object properties
@@ -81,6 +109,11 @@ creditsButton.position.x = 550;
 creditsButton.position.y = 500; 
 creditsButton.anchor.x = 0.5; 
 creditsButton.anchor.y = 0.5;  
+
+titleButton.position.x = 95; 
+titleButton.position.y = 30; 
+titleButton.anchor.x = 0.5; 
+titleButton.anchor.y = 0.5; 
 
 cannon.anchor.x = 0.5; 
 cannon.anchor.y = 0.5; 
@@ -120,8 +153,7 @@ function shootBullet( e )
 		
 		screen++; 
 		stage.removeChild( titleScreen ); 
-		
-		/*
+
 		if( bulletShotFlag == false )
 			{
 		
@@ -129,9 +161,97 @@ function shootBullet( e )
 			
 			bulletShotFlag = true; 
 			}
-			*/
 		}
 	}	
+	
+/***************************************************
+	Menu Button Functions 
+****************************************************/
+	
+/*
+* Desc: function that changes the screen to the game
+*/ 
+function startButtonClickHandler( e )
+	{
+		
+	gameVisited = true; 
+	stage.removeChild( titleScreen );
+	stage.addChild( gameplayScreen ); 
+	gameplayScreen.addChild( titleButton ); 
+	
+	renderer.backgroundColor = 0xffb18a; 
+	}
+	
+/*
+* Desc: Function that changes the screen to the tutorial 
+*/ 
+function tutorialButtonClickHandler( e )
+	{
+		
+	tutorialVisited = true; 
+	stage.removeChild( titleScreen );
+	stage.addChild( tutorialScreen );
+	
+	tutorialScreen.addChild( titleButton ); 	
+	renderer.backgroundColor = 0x7dadff;
+	}
+	
+/*
+* Desc: Function that changes the screen to the credits
+*/ 
+function creditsButtonClickHandler( e )
+	{
+		
+	creditsVisited = true; 
+	stage.removeChild( titleScreen );
+	stage.addChild( creditsScreen ); 
+	
+	renderer.backgroundColor = 0xff759c;
+	
+	var creditsText = new PIXI.Text( "Made by Joshus Tenakhongva for CS413\nSpecial Thanks to Natalie Wynn whose videos I listenened \nto while doing this" ); 
+	creditsText.position.x = 400; 
+	creditsText.position.y = 300; 
+	creditsText.anchor.x = 0.5; 
+	creditsText.anchor.y = 0.5; 
+	
+	creditsScreen.addChild( titleButton ); 
+	creditsScreen.addChild( creditsText ); 
+	}
+	
+/*
+* Desc: Function that changes the screen back to the title screen
+*/ 
+function titleMenuButtonClickHandler( e )
+	{
+
+	if( gameVisited ) 
+		{ 
+		stage.removeChild( gameplayScreen ); 
+		gameVisited = false; 
+		}
+	if( creditsVisited ) 
+		{ 
+		stage.removeChild( creditsScreen ); 
+		creditsVisited = false; 
+		}
+	if( tutorialVisited ) 
+		{ 
+		stage.removeChild( tutorialScreen ); 
+		tutorialVisited = false; 
+		}
+		
+	/*
+	stage.removeChild( gameplayScreen ); 
+	stage.removeChild( creditsScreen ); 
+	stage.removeChild( tutorialScreen ); 
+	*/ 
+
+	stage.addChild( titleScreen ); 
+	}
+	
+/*********************************************
+	Game Functionality Functions 
+************************************************/
 
 /*
 * Desc: Spawns the bullet at the cannon's position and sets the rotation to 
